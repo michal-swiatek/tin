e8a1ea8af47907f4a3f0f3e309daf3ce6239c35f
UlaTwo <44262447+UlaTwo@users.noreply.github.com>
2021-01-09 18:43:56 +0100

    Update Requests.h

--- utilities/Requests.h
+++ utilities/Requests.h
@@ -3,42 +3,6 @@
 
 // TODO: undefined command
 
-const int CLIENT_OPEN_FILE=1;
-const int SERVER_OPEN_FILE=2;
-
-const int CLIENT_READ_FILE=3;
-const int SERVER_READ_FILE=4;
-
-const int CLIENT_WRITE_FILE=5;
-const int SERVER_WRITE_FILE=6;
-
-const int CLIENT_LSEEK_FILE=7;
-const int SERVER_LSEEK_FILE=8;
-
-const int CLIENT_CLOSE_FILE=9;
-const int SERVER_CLOSE_FILE=10;
-
-const int CLIENT_UNLINK_FILE=11;
-const int SERVER_UNLINK_FILE=12;
-
-const int CLIENT_STAT_FILE=13;
-const int SERVER_STAT_FILE=14;
-
-const int CLIENT_OPEN_SESSION=15;
-const int SERVER_OPEN_SESSION=16;
-
-const int CLIENT_CLOSE_SESSION=17;
-const int SERVER_CLOSE_SESSION=18;
-
-const int CLIENT_OPEN_DIR=19;
-const int SERVER_OPEN_DIR=20;
-
-const int CLIENT_READ_DIR=21;
-const int SERVER_READ_DIR=22;
-
-const int CLIENT_CLOSE_DIR=23;
-const int SERVER_CLOSE_DIR=24;
-
 enum Request: char {
     C_OPEN_FILE = 1,
     S_OPEN_FILE = 2,
@@ -52,31 +16,31 @@ enum Request: char {
     C_FILE_LSEEK = 7,
     S_FILE_LSEEK = 8,
 
-        C_WRITE_FILE,
-        S_WRITE_FILE,
+    C_CLOSE_FILE = 9,
+    S_CLOSE_FILE = 10,
 
-        C_UNLINK_FILE,
-        S_UNLINK_FILE,
+    C_UNLINK_FILE = 11,
+    S_UNLINK_FILE = 12,
 
-        C_FILE_STAT,
-        S_FILE_STAT,
+    C_FILE_STAT = 13,
+    S_FILE_STAT = 14,
 
-        C_FILE_LSEEK,
-        S_FILE_LSEEK,
+    C_CONNECT = 15,
+    S_CONNECT = 16,
 
-        C_CLOSE_FILE,
-        S_CLOSE_FILE,
+    C_DISCONNECT = 17,
+    S_DISCONNECT= 18,
 
-        C_OPEN_DIR,
-        S_OPEN_DIR,
+    C_OPEN_DIR = 19,
+    S_OPEN_DIR = 20,
 
-        C_READ_DIR,
-        S_READ_DIR,
+    C_READ_DIR = 21,
+    S_READ_DIR = 22,
 
-        C_CLOSE_DIR,
-        S_CLOSE_DIR,
+    C_CLOSE_DIR = 23,
+    S_CLOSE_DIR = 24,
 
-        REPEAT
+    REPEAT
 };
 
 #endif //TIN_REQUESTS_H
