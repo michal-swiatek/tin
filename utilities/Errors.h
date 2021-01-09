0703a72b1d6c47f8f7109d49ef4baac5635089cb
UlaTwo <44262447+UlaTwo@users.noreply.github.com>
2021-01-09 18:47:20 +0100

    Update Errors.h

--- utilities/Errors.h
+++ utilities/Errors.h
@@ -5,6 +5,19 @@
 //  TODO: zaimplementowac kody bledow klienta
 
 enum Errors {
+    NO_ERROR = 0,
+    OTHER_ERROR=1,
+    FILE_NOT_EXISTS ,
+    INVALID_DESCRIPTOR,
+    NOT_OPEN_DESCRIPTOR,
+    INVALID_FLAG_VALUE,
+    BAD_LOGIN,
+    FOLDER_NOT_EXISTS,
+    FILE_NOT_PERMITED,
+    FILE_WRITE_ONLY,
+    FILE_READ_ONLY,
+    FILE_NOT_UNLINKED,
+
     INVALID_COMMAND,
     SERVER_ERROR
 };
